c2092b5cf2a56a9d16746c72afa970b17b5d97b0
“Dankyi <dankyibenjamin0@gmail.com>
2024-11-18 22:07:57 +0000

    Authentication Phase

--- management/views.py
+++ management/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
 # Create your views here.
 
 
+@login_required
 def home(request):
-    return HttpResponse("this works")
+    return render(request, 'management/home.html')
+
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'management/register.html', {'form': form})
--- project_management_app/settings.py
+++ project_management_app/settings.py
@@ -140,3 +140,9 @@ TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# custom redirect URLS
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+# remember both forward slashes
+LOGOUT_REDIRECT_URL = 'login'
